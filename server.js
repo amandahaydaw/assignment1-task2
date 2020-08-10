2a8d8aaeeea2e64299dbd715da25cdb7dcb2a8d4
amanda.haydaw <amanda.haydaw@accenture.com>
2020-08-10 15:23:11 +1000

    changing ui

--- server.js
+++ server.js
@@ -16,7 +16,29 @@ app.get('/map', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 
 });
+//collection to handle messages
+let collectionMessages;
+//database connection 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://Uber:123A@cluster0.0ncih.mongodb.net/DBSIT725?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
+
+
+client.connect(err => {
+    collectionMessages = client.db("DBSIT725").collection("messages");
+});
+
+
+//create function message to insert data into db
+const InsertMessage=(message)=>{
+    collectionMessages.insertOne({message:message})
+}
+
+
+//setting time out 
+setTimeout(function(){
+    InsertMessage('SETTING TIME OUT')},10000);
 
 //port
 let port = 3030;
